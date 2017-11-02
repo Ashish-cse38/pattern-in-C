0e203b7d4703b09074bac66f7857e1306b78ede8
Ashish-cse38 <33326109+Ashish-cse38@users.noreply.github.com>
2017-11-02 22:48:32 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,l;
+for(i=1;i<=5;i++)
+{
+printf("\n");
+for(j=1;j<=6-i;j++)
+{
+printf(" ");
+}
+for(j=1;j<=2*i-1;j++)
+{
+printf("*");
+}
+}
+for(k=1;k<=5;k++)
+{
+printf("\n");
+for(l=1;l<=k;l++)
+{
+printf(" ");
+}
+for(l=1;l<=11-(2*k);l++)
+{
+printf("*");
+}
+}
+}
